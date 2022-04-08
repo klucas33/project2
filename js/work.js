c248e18ca6b98fada07554b4cf8ac05b71679f8f
klucas33 <klucas96@naver.com>
2022-04-09 00:23:02 +0900

    change

--- js/work.js
+++ js/work.js
@@ -1,44 +1,51 @@
-$(function(){
-    setTimeout(function(){
-        $('.page_hidden div').css({'transform':'translateY(0)'})
-        $('.page_hidden svg').css({'transform':'translate(0)'})
-      },250)
-      setTimeout(function(){
-        $('.page_hidden').css({'z-index':'0'})
-        $('.page_hidden div').css({'transform':'translateY(-100%)'})
-        
-      },800)
+$(function () {
+  setTimeout(function () {
+    $(".page_hidden div").css({ transform: "translateY(0)" });
+    $(".page_hidden svg").css({ transform: "translate(0)" });
+  }, 250);
+  setTimeout(function () {
+    $(".page_hidden").css({ "z-index": "0" });
+    $(".page_hidden div").css({ transform: "translateY(-100%)" });
+  }, 800);
 
-      var clone = ['brio toys -','Sparbanken Skåne -','Tetra Pak Services -','Oatly -','MECA -','Prisjakt -'];
-      
-  
-     
-      $('.same').each(function(k){
-        var txt='';
+  var clone = [
+    "brio toys -",
+    "Sparbanken Skåne -",
+    "Tetra Pak Services -",
+    "Oatly -",
+    "MECA -",
+    "Prisjakt -",
+  ];
 
-        for(var i=0;i<10;i++){
-          txt += '<strong>'+clone[k]+'</strong>';
-        }
-        $(this).find('p').html(txt);
-      })
-      
-   
-      setTimeout(function(){
-        var num = 0
-        var width = $('.same').find('strong').width()
-        var cold = width * 5;
-        
-        setInterval(function(){
-          num +=.5;
-          if(cold<num)num=0;
-          $('.same:even a p').css('transform','translateX(-'+num+'px)')
-          $('.same:odd a p').css('transform','translateX('+num+'px)')
-        },10)
-      },100)
+  $(".same").each(function (k) {
+    var txt = "";
+    for (var i = 0; i < 10; i++) {
+      txt += "<strong>" + clone[k] + "</strong>";
+    }
+    $(this).find("p").html(txt);
+  });
 
-      
-      
+  setTimeout(function () {
+    var num = 0;
+    var width = $(".same").find("strong").width();
+    var cold = width * 5;
 
+    setInterval(function () {
+      num += 0.5;
+      if (cold < num) num = 0;
+      $(".same:odd a p").css("transform", "translateX(" + -num + "px)");
+    }, 10);
+  }, 100);
 
-      
-})
+  setTimeout(function () {
+    var num = 0;
+    var width = $(".same").find("strong").width();
+    var cold = width * 5;
+
+    setInterval(function () {
+      num += 0.5;
+      if (cold < num) num = 0;
+      $(".same:even a p").css("transform", "translateX(-" + num + "px)");
+    }, 10);
+  }, 100);
+});
